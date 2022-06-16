d329754f93894a9885d1a683ff982ced12a5d8b9
Kate Vorobeva <ekvorobeva@dengabank.ru>
2022-06-16 18:20:22 +0300

    Add the Filmwork model to the models.py.

--- 02_movies_admin/movies/models.py
+++ 02_movies_admin/movies/models.py
@@ -13,3 +13,22 @@ class Genre(models.Model):
         db_table = '"content"."genre"'
         verbose_name = 'Жанр'
         verbose_name_plural = 'Жанры'
+
+
+class Filmwork(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.TextField('title')
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateField('creation_date')
+    rating = models.FloatField('rating', blank=True)
+    type = models.TextField('type', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = '"content"."film_work"'
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+    # def __str__(self):
+    #     return self.title
