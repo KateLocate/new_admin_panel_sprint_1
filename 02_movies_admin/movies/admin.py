fea8c71f939fcc0f3f01ad6dea5f8964347637d9
Kate Vorobeva <ekvorobeva@dengabank.ru>
2022-06-17 16:25:12 +0300

    Add the Person and PersonFilmwork models to the admin panel.

--- 02_movies_admin/movies/admin.py
+++ 02_movies_admin/movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Genre, Filmwork, GenreFilmwork
+from .models import Genre, Filmwork, GenreFilmwork, Person, PersonFilmwork
 
 
 @admin.register(Genre)
@@ -8,13 +8,23 @@ class GenreAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description', 'id')
 
 
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'created', 'modified')
+    search_fields = ('full_name', 'id')
+
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
 
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
-    inlines = (GenreFilmworkInline,)
+    inlines = (GenreFilmworkInline, PersonFilmworkInline)
 
     list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
     list_filter = ('type', 'rating', 'genres')
