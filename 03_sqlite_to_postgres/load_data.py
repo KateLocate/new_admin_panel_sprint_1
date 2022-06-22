0337dc7c47c9ae075e640450b01a21f88bb8a843
Kate Vorobeva <ekvorobeva@dengabank.ru>
2022-06-22 17:18:04 +0300

    Fix: replace sensitive data with enviroment variables.

--- 03_sqlite_to_postgres/load_data.py
+++ 03_sqlite_to_postgres/load_data.py
@@ -6,7 +6,7 @@ from psycopg2.extras import DictCursor
 
 
 def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
-    """Основной метод загрузки данных из SQLite в Postgres"""
+    """Main function to transfer data from SQLite to Postgres"""
     # postgres_saver = PostgresSaver(pg_conn)
     # sqlite_loader = SQLiteLoader(connection)
 
@@ -15,6 +15,17 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
 
 
 if __name__ == '__main__':
-    dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
+    import os
+
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    dsl = {
+        'dbname': os.environ.get('DB_NAME'),
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASSWORD'),
+        'host': '127.0.0.1',
+        'port': os.environ.get('DB_PORT'),
+    }
     with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
         load_from_sqlite(sqlite_conn, pg_conn)
